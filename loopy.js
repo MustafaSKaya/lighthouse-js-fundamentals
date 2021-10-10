ad3a12cfdcc8449f128c920921c46272bf0b6935
MustafaSKaya <mustafakya88@hotmail.com>
2021-10-10 23:19:18 +0000

    loopy.js

--- loopy.js
+++ loopy.js
@@ -5,3 +5,23 @@ for (x = 100; x <= 200; x++) {
         console.log(x);
     } 
 }
+const stations = [
+    ['Big Bear Donair', 10, 'restaurant'],
+    ['Bright Lights Elementary', 50, 'school'],
+    ['Moose Mountain Community Centre', 45, 'community centre']
+  ];
+  function chooseStations(stations) {
+    let goodStations = []
+    for (const station of stations) {
+      const capacity = station[1];
+      if (capacity >= 20) {
+        const type = station[2];
+        if (type === "school" || type === "community centre") {
+          goodStations.push(station[0]);
+        }
+      }
+    }// Your code in here ...
+  
+    return goodStations
+  }
+  console.log(chooseStations(stations));
